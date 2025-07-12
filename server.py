7383ac7a4bc93371dc25e73207c32743b0e49b27
மனோஜ்குமார் பழனிச்சாமி <smartmanoj42857@gmail.com>
2025-07-12 21:43:26 +0530

    test

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
+import os
+
+import requests
 from fastmcp import FastMCP
 
-mcp = FastMCP("Demo 🚀")
+token = os.getenv('TELEGRAM_BOT_TOKEN')
+chat_id = os.getenv('TELEGRAM_CHAT_ID')
+mcp = FastMCP("Simple Telegram Bot MCP")
+url = f"https://api.telegram.org/bot{token}/sendMessage"
 
 @mcp.tool
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def send_telegram_message(text: str) -> str:
+    """Send a message to a Telegram chat using the simple send_message function"""
+    try:
+        # requests.get(url, params={'chat_id': chat_id, 'text': text})
+        return 'success'
+    except Exception as e:
+        return f"Error sending message: {str(e)}"
 
 if __name__ == "__main__":
     mcp.run()
